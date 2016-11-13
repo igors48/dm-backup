15efc6195700533a97d93944136d001c52951fb9
Igor Usenko <nmds48@gmail.com>
2016-11-13 17:13:24 +0200

    cron setup

--- src/main/java/war/CronServlet.java
+++ src/main/java/war/CronServlet.java
@@ -22,7 +22,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloAppEngine extends HttpServlet {
+public class CronServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
