5c543ada771449359ec9304299eb2bfe4ae429e1
Igor Usenko <nmds48@gmail.com>
2016-11-14 21:12:04 +0200

    initial test

---
+++ src/test/java/Test.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by igor on 14.11.2016.
+ */
+public class Test {
+
+    @org.junit.Test
+    public void name() throws Exception {
+        assertTrue(true);
+
+    }
+}
