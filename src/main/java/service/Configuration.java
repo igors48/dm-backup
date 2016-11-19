6e1117dbdb5f2d621108be9c697e102e0be77d0e
Igor Usenko <nmds48@gmail.com>
2016-11-19 14:20:04 +0200

    configuration

---
+++ src/main/java/service/Configuration.java
@@ -0,0 +1,23 @@
+package service;
+
+/**
+ * Created by igor on 19.11.2016.
+ */
+public class Configuration {
+
+    private static final String ORIGIN = "origin";
+    private static final String HOST = "host";
+
+    private static final String LOGIN_URL = "login.url";
+    private static final String LOGIN_REFERER = "login.referer";
+    private static final String LOGIN_DATA = "login.data";
+
+    private static final String DOWNLOAD_URL = "download.url";
+    private static final String DOWNLOAD_REFERER = "download.referer";
+    private static final String DOWNLOAD_DATA = "download.data";
+
+    private static final String ADMIN = "admin";
+    private static final String RECIPIENTS = "recipients";
+}
+
+//System.getProperty(TWITTER_API_KEY);
