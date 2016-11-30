ded07ada49063d7a2dcf824edc877c9ad7ff6070
Igor Usenko <nmds48@gmail.com>
2016-11-30 22:30:12 +0200

    #1 changes detector. dependencies

--- src/main/java/gae/Dependencies.java
+++ src/main/java/gae/Dependencies.java
@@ -1,12 +1,16 @@
 package gae;
 
-import gae.repository.snapshot.GaeSnapshotRepository;
-import gae.repository.timestamp.GaeTimestampRepository;
 import gae.repository.GaeTransactions;
+import gae.repository.snapshot.GaeSnapshotRepository;
+import gae.repository.snapshot.SnapshotConverter;
+import gae.repository.timestamp.GaeTimestampRepository;
+import gae.repository.timestamp.TimestampConverter;
 import gae.service.GaeTimeService;
 import service.*;
 import service.configuration.Configuration;
-import service.error.ServiceException;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by igor on 20.11.2016.
@@ -15,18 +19,39 @@ public enum Dependencies {
 
     GET;
 
-    public Backup backupService() throws ServiceException {
-        final Configuration configuration = Configuration.fromSystemProperties();
+    private static final Logger LOGGER = Logger.getLogger(Dependencies.class.getName());
 
-        final Loader loader = new Loader(configuration.getAccessParameters());
-        final Sender sender = new Sender();
-        final Transactions transactions = GaeTransactions.INSTANCE;
-        final SnapshotRepository snapshotRepository = GaeSnapshotRepository.create();
-        final TimestampRepository timestampRepository = GaeTimestampRepository.create();
-        final TimeService timeService = GaeTimeService.INSTANCE;
-        final ChangesDetector changesDetector = new ChangesDetector(snapshotRepository, timestampRepository, timeService, 48, transactions);
+    private static Configuration configuration;
+    private static Loader loader;
+    private static Sender sender;
+    private static Transactions transactions;
+    private static SnapshotRepository snapshotRepository;
+    private static TimestampRepository timestampRepository;
+    private static TimeService timeService;
+    private static ChangesDetector changesDetector;
+    private static Backup backup;
 
-        return new Backup(loader, sender, changesDetector, configuration.getRecipients());
+    static {
+
+        try {
+
+            configuration = Configuration.fromSystemProperties();
+            loader = new Loader(configuration.getAccessParameters());
+            sender = new Sender();
+            transactions = GaeTransactions.INSTANCE;
+            snapshotRepository = new GaeSnapshotRepository(SnapshotConverter.SNAPSHOT_CONVERTER);
+            timestampRepository = new GaeTimestampRepository(TimestampConverter.TIMESTAMP_CONVERTER);
+            timeService = GaeTimeService.INSTANCE;
+            changesDetector = new ChangesDetector(snapshotRepository, timestampRepository, timeService, 48, transactions);
+            backup = new Backup(loader, sender, changesDetector, configuration.getRecipients());
+
+        } catch (Exception exception) {
+            LOGGER.log(Level.SEVERE, "Application initialization error", exception);
+        }
+    }
+
+    public Backup backupService() {
+        return backup;
     }
 
 }
