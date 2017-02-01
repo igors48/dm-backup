b501bc1e9584f1a71b128f2dea63f6aa1969cc44
Igor Usenko <nmds48@gmail.com>
2017-02-01 23:14:38 +0200

    #5 thymeleaf smoke

--- src/main/java/service/Template.java
+++ src/main/java/service/Template.java
@@ -2,8 +2,13 @@ package service;
 
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-import org.thymeleaf.templateresolver.ITemplateResolver;
+import util.account.Account;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by igor on 01.02.2017.
@@ -13,12 +18,23 @@ public class Template {
     private final TemplateEngine engine;
 
     public Template() {
-        ITemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setPrefix("templates\\");
+        templateResolver.setSuffix(".txt");
+
         this.engine = new TemplateEngine();
         this.engine.setTemplateResolver(templateResolver);
     }
 
     public String formatContent() {
-        return this.engine.process("templates\\content.txt", new Context());
+        final Context context = new Context(Locale.ROOT);
+        context.setVariable("backupTime", "12:34");
+        context.setVariable("serverName", "wonderserver");
+
+        List<Account> accounts = Arrays.asList(new Account("a", "b"), new Account("c", "d"));
+        context.setVariable("accounts", accounts);
+
+        return this.engine.process("content", context);
     }
 }
