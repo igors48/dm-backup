79da7b0a2fec53650461e22f76f20a757534c4ee
Igor Usenko <nmds48@gmail.com>
2017-02-17 23:21:42 +0200

    #1 backup content store tests

--- src/test/java/BackupContentStoreTest.java
+++ src/test/java/BackupContentStoreTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 import service.Action;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.*;
 
 /**
@@ -47,6 +48,36 @@ public class BackupContentStoreTest extends BackupTestBase {
         this.backup.execute();
 
         verifyZeroInteractions(this.contentStore);
+    }
+
+    @Test
+    public void whenActionIsSaveAndContentStoreFailedThenTransactionRolledBack() {
+
+        try {
+            when(this.changesDetector.getActionForContent(CONTENT.file)).thenReturn(Action.SAVE);
+            doThrow(new RuntimeException()).when(this.contentStore).store(CONTENT);
+
+            this.backup.execute();
+
+            fail();
+        } catch (Exception e) {
+            assertEquals(TransactionState.ROLLED_BACK, this.transactionStub.getState());
+        }
+    }
+
+    @Test
+    public void whenActionIsUpdateAndContentStoreFailedThenTransactionRolledBack() {
+
+        try {
+            when(this.changesDetector.getActionForContent(CONTENT.file)).thenReturn(Action.UPDATE_LAST);
+            doThrow(new RuntimeException()).when(this.contentStore).updateLast(CONTENT);
+
+            this.backup.execute();
+
+            fail();
+        } catch (Exception e) {
+            assertEquals(TransactionState.ROLLED_BACK, this.transactionStub.getState());
+        }
     }
 
 }
