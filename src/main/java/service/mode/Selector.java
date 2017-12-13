a373312ec02e523d2cc25b8cfd4f576034c96146
Igor Usenko <nmds48@gmail.com>
2017-12-13 21:16:08 +0200

    #77 alarm commit

--- src/main/java/service/mode/Selector.java
+++ src/main/java/service/mode/Selector.java
@@ -1,11 +1,12 @@
 package service.mode;
 
 import com.google.appengine.api.datastore.Transaction;
-import service.Content;
-import service.Loader;
-import service.Sender;
-import service.Transactions;
+import org.joda.time.DateTime;
+import service.*;
 import service.error.ServiceException;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static util.Assert.guard;
 import static util.Parameter.notNull;
@@ -13,17 +14,21 @@ import static util.TransactionTools.rollbackIfActive;
 
 public class Selector {
 
+    private static final Logger LOGGER = Logger.getLogger(Selector.class.getName());
+
     private final Configuration configuration;
     private final Loader loader;
     private final Sender sender;
     private final CronJobStateStore cronJobStateStore;
+    private final TimeService timeService;
     private final Transactions transactions;
 
-    public Selector(final Configuration configuration, final Loader loader, final Sender sender, final CronJobStateStore cronJobStateStore, final Transactions transactions) {
+    public Selector(final Configuration configuration, final Loader loader, final Sender sender, final CronJobStateStore cronJobStateStore, final TimeService timeService, final Transactions transactions) {
         guard(notNull(this.configuration = configuration));
         guard(notNull(this.loader = loader));
         guard(notNull(this.sender = sender));
         guard(notNull(this.cronJobStateStore = cronJobStateStore));
+        guard(notNull(this.timeService = timeService));
         guard(notNull(this.transactions = transactions));
     }
 
@@ -49,14 +54,22 @@ public class Selector {
             transaction.commit();
 
             if (errorMailShouldSent) {
-
+                sendException(exception);
             }
-
-        } catch (Exception e) {
 
         } finally {
             rollbackIfActive(transaction);
         }
+    }
+
+    private void sendException(ServiceException exception) {
+
+        try {
+            this.sender.sendException(this.configuration.recipients.adminRecipient, exception);
+        } catch (ServiceException e) {
+            LOGGER.log(Level.SEVERE, "Error mail do not send", e);
+        }
+
     }
 
     private boolean checkErrorMailShouldSent(CronJobState cronJobState) {
@@ -85,6 +98,19 @@ public class Selector {
         } finally {
             rollbackIfActive(transaction);
         }
+    }
+
+    private boolean itIsRushTime() {
+        final long currentTimestamp = this.timeService.currentTimestamp();
+        final DateTime current = new DateTime(currentTimestamp);
+        final int currentHour = current.getHourOfDay();
+
+        final int startHour = this.configuration.rushHour;
+
+        final int endPeriod = this.configuration.rushHour + this.configuration.rushPeriod;
+        final int endHour = endPeriod > 23 ? endPeriod - 23 : endPeriod;
+
+        return false;
     }
 
 }
